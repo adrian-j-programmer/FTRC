e60ba87bb6ca6bc47b77e9ef53b4497fb74725ec
adrian.jakubiak.programmer <adrian.jakubiak.programmer@gmail.com>
2019-06-30 10:31:11 +0200

    Fix compilation error (paths too long)

--- FlexibleTrainingRegimenCompanion/FTRC/MainPage.xaml.cs
+++ FlexibleTrainingRegimenCompanion/FTRC/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace FlexibleTrainingRegimenCompanion
+namespace FTRC
 {
     public partial class MainPage : ContentPage
     {
